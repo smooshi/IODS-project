657669d9ac4fe2c30952a0d5a22c976e8a5a9fa9
smooshi <smooshi@users.noreply.github.com>
2018-12-02 11:38:59 +0200

    Chap 5 DW
    Data Wrangling

--- data/create_human.R
+++ data/create_human.R
@@ -1,3 +1,8 @@
+library(dplyr)
+library(stringr)
+library(GGally)
+#Week 4 datawrangling
+
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
@@ -29,3 +34,44 @@ gii["edu2Ratio"] <- gii["Edu2F"]/gii["Edu2M"]
 gii["LabRatio"] <- gii["LabF"]/gii["LabM"]
 
 joined_data <- inner_join(hd, gii, by="Country")
+
+#Week 5 Data wrangling
+human <- joined_data
+
+#Make GNI numeric
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
+
+#Remove columns you don't need
+keep <- c("Country", "Edu2F", "LabRatio", "LifeExpectancy", "EYE", "GNI", "MaternalMortality", "Adolescent.Birth.Rate", "Parliament")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human))
+
+# last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+
+#remove Country
+keep <- c("Edu2F", "LabRatio", "LifeExpectancy", "EYE", "GNI", "MaternalMortality", "Adolescent.Birth.Rate", "Parliament")
+human <- select(human_, one_of(keep))
+
+#Write
+write.csv(human, file="~/GitHub/IODS-project/data/human.csv", row.names = TRUE)
+
+test <- read.table("~/GitHub/IODS-project/data/human.csv", header = TRUE, row.names= 1, sep = ",")
+head(test)
+str(test)
